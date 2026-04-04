917f4623a32c53553f6ff3c6391fc4d38bf2bd5e
Julia <hi@juliab.dev>
2026-04-03 20:42:37 -0600

    format time

--- src/formatTime.ts
+++ src/formatTime.ts
@@ -5,5 +5,13 @@
  * @returns readable label, ex. Less than a minute, 20 minutes, 2 hours
  */
 export function formatTime(minutes: number): string {
-  return "";
+  if (minutes < 1) return "Less than a minute";
+  if (minutes < 2) return "1 minute";
+  if (minutes < 10) return `${Math.floor(minutes)} minutes`;
+  if (minutes < 59) return `${Math.floor(minutes / 5) * 5} minutes`;
+  if (minutes < 90) return "1 hour";
+  if (minutes < 120) return "1.5 hours";
+  if (minutes < 150) return "2 hours";
+  if (minutes < 180) return "2.5 hours";
+  return "A few hours";
 }
--- tests/api.test.ts
+++ tests/api.test.ts
@@ -44,14 +44,21 @@ describe("readingTime", () => {
 describe("formatTime", () => {
   test("formats a minute", () => {
     expect(formatTime(1)).toBe("1 minute");
+    expect(formatTime(1.5)).toBe("1 minute");
   });
 
   test("formats multiple minutes", () => {
+    expect(formatTime(13)).toBe("10 minutes");
     expect(formatTime(15)).toBe("15 minutes");
+    expect(formatTime(17)).toBe("15 minutes");
+    expect(formatTime(29)).toBe("25 minutes");
+    expect(formatTime(45.67)).toBe("45 minutes");
   });
 
   test("formats less than a minute", () => {
     expect(formatTime(0.04)).toBe("Less than a minute");
+    expect(formatTime(0.2)).toBe("Less than a minute");
+    expect(formatTime(0.12345)).toBe("Less than a minute");
   });
 
   test("returns less than a minute for edge cases", () => {
@@ -60,18 +67,23 @@ describe("formatTime", () => {
   });
 
   test("formats an hour", () => {
+    expect(formatTime(59.9)).toBe("1 hour");
     expect(formatTime(60)).toBe("1 hour");
+    expect(formatTime(89)).toBe("1 hour");
   });
 
   test("formats half hours", () => {
     expect(formatTime(90)).toBe("1.5 hours");
+    expect(formatTime(100)).toBe("1.5 hours");
   });
 
   test("formats a long reading time", () => {
     expect(formatTime(120)).toBe("2 hours");
+    expect(formatTime(150)).toBe("2.5 hours");
   });
 
   test("formats a really long reading time", () => {
+    expect(formatTime(180)).toBe("A few hours");
     expect(formatTime(200)).toBe("A few hours");
   });
 });
